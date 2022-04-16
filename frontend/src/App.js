d682ce7a7674579f93cc9074d4005e1fdf75759b
Charles Calapini <7816568+calapsss@users.noreply.github.com>
2022-04-16 02:32:31 -0600

    basic handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 
 import GoalList from './components/GoalList';
+import NewGoal from './components/NewGoal';
 
 function App() {
-  const goals = [
-    {id: 'cg1', text: 'Finish Course'},
-    {id: 'cg2', text: 'Learn main tools'},
-];
 
+  const [goals, setGoals] = useState(
+    [
+      {id: 'cg1', text: 'Finish Course'},
+    {id: 'cg2', text: 'Learn main tools'},
+    ]
+  );
+ 
+
+
+
+  const addNewGoalHandler = (newGoal) => {
+    setGoals(goals.concat(newGoal));
+    console.log(goals)
+
+  }
 
 
   return (
     <div className = "goals">
       <h2>Course Goals </h2>
+        <NewGoal  onAddGoal={addNewGoalHandler} />
         <GoalList goals={goals}/>
     </div>
   );
--- frontend/src/components/NewGoal.js
+++ frontend/src/components/NewGoal.js
@@ -1,3 +1,32 @@
 import React from 'react';
 
+function NewGoal(props){
+
+    const addGoalHandler = event =>{
+        event.preventDefault();
+        
+        const newGoal = {
+            id: Math.random().toString(),
+            text: "My new goal"
+        };
+        props.onAddGoal(newGoal);
+
+    }
+
+    return(
+        <>
+            <form className = "new-goal" onSubmit={addGoalHandler}>
+                <input type="text" />
+
+                <button type="submit" >Add Goal</button>
+            </form>
+        
+        </>
+    )
+
+
+}
+
+
+export default NewGoal;
 
